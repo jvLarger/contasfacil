a265ebb0a50fb4dc04fe5347d861f709dcf737ba
João Victor Larger <larger13@hotmail.com>
2023-03-25 09:25:41 -0300

    Iniciando projeto

---
+++ src/main/java/com/jlarger/contasfacil/ContasfacilApplication.java
@@ -0,0 +1,13 @@
+package com.jlarger.contasfacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContasfacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContasfacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jlarger/contasfacil/ContasfacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jlarger.contasfacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContasfacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
